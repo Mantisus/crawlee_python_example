78f38e7417b103c33ce33a561e10d645539958e0
Max Bohomolov <moriturus7@gmail.com>
2024-08-19 15:45:45 +0000

    save dataset results to one file

--- src/afs_crowlee/main.py
+++ src/afs_crowlee/main.py
@@ -1,13 +1,15 @@
+from crawlee.storages.dataset import Dataset
+
 from .custom_crawler import CustomCrawler
 from .router import router
 
 
 async def main() -> None:
     """The main function that starts crawling."""
-    crawler = CustomCrawler(
-        max_requests_per_crawl=50,
-        request_handler=router,
-    )
+    crawler = CustomCrawler(max_requests_per_crawl=50, request_handler=router)
 
     # Run the crawler with the initial list of URLs.
     await crawler.run(['https://www.accommodationforstudents.com/'])
+
+    dataset = await Dataset.open()
+    await dataset.export_to(key='properties', content_type='json', to_key_value_store_name='results')
